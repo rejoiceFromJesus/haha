1c470e57b0325230d2e57d72d4f1d635149fbe53
rejoiceFromJesus <948870341@qq.om>
2017-10-24 09:26:33 +0800

    first import

---
+++ rejoice-blog/src/main/java/com/rejoice/blog/RejoiceBlogApplication.java
@@ -0,0 +1,12 @@
+package com.rejoice.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RejoiceBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RejoiceBlogApplication.class, args);
+	}
+}
